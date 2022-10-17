d79c2d2a6ad11950ae8f609c1c7b680ad14ea1cf
NEWJYH <joung5846@naver.com>
2022-10-17 17:20:32 +0900

    TodoList TodoItem render test ok

---
+++ src/TodoList.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import TodoList from './TodoList';
+
+describe('<TodoList />', () => {
+    const sampleTodos = [
+        {
+        id : 1,
+        text : 'TDD 배우기',
+        done : false,
+        },
+        {
+            id : 2,
+            text : 'react-testing-library',
+            done : true,
+        },
+    ];
+    it('has two todos', () => {
+        const {getByText} = render(<TodoList todos={sampleTodos}/>);
+        getByText(sampleTodos[0].text);
+        getByText(sampleTodos[1].text);
+    });
+});
